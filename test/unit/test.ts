3b064f54e7f539ddcb4dd5af742360eb8603fe23
alex <alex@activewidgets.com>
2018-02-23 00:30:41 +0100

    add testbed test

--- test/unit/test.ts
+++ test/unit/test.ts
@@ -1,6 +1,27 @@
 
-import {Grid} from '../../';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
+
+import {AxModule, Grid} from '../../';
 
 test('test 1', () => {
     expect(typeof Grid).toBe('function');
+});
+
+describe('ng testbed', () => {
+
+    let grid, fixture, el;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [AxModule]
+        });
+
+        fixture = TestBed.createComponent(Grid);
+        el = fixture.nativeElement;
+    });
+
+    it('css', (){
+        expect(el.className).toBe('ax-grid');
+    });
 });
