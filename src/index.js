07d14c395ca0395fb29c31e752a9a875fb3789a2
alex <alex@activewidgets.com>
2019-03-25 12:56:13 +0100

    remove angular helper classes

--- src/index.js
+++ src/index.js
@@ -11,13 +11,12 @@ let declarations = [],
 
 export const Grid = build(grid);
 export const Row = build(row);
-export const Slot = AngularBuilder.Slot;
 export const ForOf = AngularBuilder.ForOf;
 
 export class  AxModule {
     static get annotations() { return [new core.NgModule({
         imports: [CommonModule],
-        exports: [Slot, ForOf, Grid, Row],
+        exports: [ForOf, Grid, Row],
         declarations
     })]}
 }
