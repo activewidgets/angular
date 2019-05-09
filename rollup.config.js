cab3ab643e323d53548d6335d9c4db2b099f044d
alex <alex@activewidgets.com>
2019-05-09 19:08:56 +0200

    use ActiveWidgets global

--- rollup.config.js
+++ rollup.config.js
@@ -4,14 +4,14 @@ import babel from 'rollup-plugin-babel';
 
 let globals = {
     '@angular/core': 'ng.core',
-    '@activewidgets/frameworks/angular': 'AX.frameworks.angular',
-    '@activewidgets/grid': 'AX.components'
+    '@activewidgets/frameworks/angular': 'ActiveWidgets.frameworks.angular',
+    '@activewidgets/grid': 'ActiveWidgets.components'
 };
 
 export default {
     input: 'index.js',
     output: [
-        {file: 'dist/ax-angular.js', format: 'umd', sourcemap: true, name: 'AX.Angular', extend: true, globals},
+        {file: 'dist/ax-angular.js', format: 'umd', sourcemap: true, name: 'ActiveWidgets.Angular', extend: true, globals},
         {file: 'dist/ax-angular.esm.js', format: 'esm', sourcemap: true}
     ],
     external: [
