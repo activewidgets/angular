db59c80786ed1cf67f629436a8ae957e7dbe8c46
alex <alex@activewidgets.com>
2019-12-13 22:43:13 +0100

    move storybook, examples, tests to separate packages

--- storybook/demo.ts
+++
@@ -1,7 +0,0 @@
-
-import {storiesOf} from '@storybook/angular';
-
-import basic from '../examples/basic';
-
-storiesOf('Demo', module)
-    .add('basic', basic);
--- storybook/e2e.ts
+++
@@ -1,7 +0,0 @@
-
-import {storiesOf} from '@storybook/angular';
-
-import basic from '../examples/basic';
-
-storiesOf('Tests', module)
-    .add('basic', basic);
